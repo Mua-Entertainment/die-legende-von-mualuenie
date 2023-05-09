0596734daea73f0fee9e269aba1b1199b8099ca6
Mua-Entertainment <mua.entertainment@outlook.com>
2023-05-09 18:01:58 +0200

    change

--- src/startup/Program.java
+++ src/startup/Program.java
@@ -1,7 +1,14 @@
 package startup;
 
+import imports.OpenSimplex2S;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("test");
+        long seed = 0;
+
+        for (int x = 0; x < 100_000f; x++) {
+            float noise = OpenSimplex2S.noise2(seed, x / 10f, 0);
+            System.out.println(noise);
+        }
     }
 }
