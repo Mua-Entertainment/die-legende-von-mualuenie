7f9eb1f9c703cebafb527c3eb0548b4f163185d2
Daronok <github@louistube.de>
2023-05-23 17:15:41 +0200

    Mua fällt besser

--- src/development/Mualuenie.java
+++ src/development/Mualuenie.java
@@ -6,9 +6,14 @@ import java.awt.event.KeyEvent;
 
 public class Mualuenie extends ImageObject {
 
-    private final float SPEED = 2f;
+    private enum State {
+        GROUND, JUMP, AIR
+    }
+    private final float SPEED = 5f;
     private final float GRAVITY = 1f;
     private float airtime = 0;
+    private State state = State.GROUND;
+
 
     @Override
     protected void load() {
@@ -30,6 +35,8 @@ public class Mualuenie extends ImageObject {
         if (getInput().keyPressed(KeyEvent.VK_S)) move(0, SPEED / getFPS());
         if (getInput().keyPressed(KeyEvent.VK_A)) move(-SPEED / getFPS(), 0);
         if (getInput().keyPressed(KeyEvent.VK_D)) move(SPEED / getFPS(), 0);
+
+
         if (getInput().keyPressed(KeyEvent.VK_SPACE)) move(0, -SPEED / getFPS());
         move (0, GRAVITY * airtime / getFPS());
         airtime = airtime + 1f/getFPS();
@@ -39,5 +46,7 @@ public class Mualuenie extends ImageObject {
     private void onCollide(GameObject other, Collision collision) {
         System.out.println(other);
         airtime = 0;
+        state=State.GROUND;
+
     }
 }
