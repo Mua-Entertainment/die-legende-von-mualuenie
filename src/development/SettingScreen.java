6027a31c97ac9900a944597ae1996291c08de4f3
jojoz <jojo.zwingmann@gmx.de>
2023-05-24 17:57:23 +0200

    sfksjf

--- src/development/SettingScreen.java
+++ src/development/SettingScreen.java
@@ -1,8 +1,16 @@
 package development;
 
+import engine.Button;
 import engine.GameObject;
 
 public class SettingScreen extends GameObject
 {
+    @Override
+    protected void load() {
+        super.load();
+        Button Back =new Button();
+        addChildren(Back);
+        Back.setSrc("pfeil.png");
+    }
 
 }
