c1af0fff445f8daa4a1031221caa3d5cb72efb63
jojoz <jojo.zwingmann@gmx.de>
2023-05-24 17:04:20 +0200

    dfhskudh

--- src/development/MainMenu.java
+++ src/development/MainMenu.java
@@ -12,13 +12,33 @@ public class MainMenu extends GameObject
         addChildren(Startbutton);
         Startbutton.setSrc("button.png");
         Startbutton.setSize(3, 1 );
-        Startbutton.label.setText("Start");
+        Startbutton.label.setText("         Start");
         Startbutton.setPosition(6,4);
         Startbutton.Click.subscribe(this::Spielstarten);
 
+        Button Settingsbutton = new Button();
+        addChildren(Settingsbutton);
+        Settingsbutton.setSrc("zahnrader.png");
+        Settingsbutton.setSize(1,1);
+        Settingsbutton.setPosition(2,4);
+        Settingsbutton.Click.subscribe(this::OpenSettings);
+
+        Button Skinbutton = new Button();
+        addChildren(Skinbutton);
+        Skinbutton.setSrc("mua5.png");
+        Skinbutton.setPosition(4,2);
+        Skinbutton.Click.subscribe(this::SkinMenu);
     }
     private void Spielstarten()
     {
         move(5, 2 );
+    }
+    private void OpenSettings()
+    {
+
+    }
+    private void SkinMenu()
+    {
+
     }
 }
