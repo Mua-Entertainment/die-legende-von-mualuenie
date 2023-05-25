9181586b2d7133603339995006a0e67c78bd366a
Daronok <github@louistube.de>
2023-05-25 09:41:46 +0200

    chunk ist jetzt cooler

--- src/development/WorldGen.java
+++ src/development/WorldGen.java
@@ -1,30 +1,25 @@
 package development;
 
 import engine.GameObject;
+import engine.RectObject;
+
 //Generiert die Welt
 public class WorldGen extends GameObject {
 
     private Chunk chunk;
-    private float time = 0f;
 
-    public float GAMESPEED = 1f;
-    private int frontTile = 0;
+    private RectObject rect;
 
 
     @Override
     protected void load() {
         super.load();
-/*
-        chunks = new Chunk[6];
-        for (int i = 0; i < 6;i++)
-        {
-            addChildren(chunks[i] = new Chunk());
-            chunks[i].setGlobalPosition(i * 2.5f,5f);
-        }
 
- */
         addChildren(chunk = new Chunk());
         chunk.setGlobalPosition(getCanvasSize().width(),5f);
+
+        addChildren(rect = new RectObject());
+        //rect.
 
     }
 
@@ -32,21 +27,6 @@ public class WorldGen extends GameObject {
     protected void update() {
         super.update();
 
-        /*
-        if (time >= GAMESPEED * 1.25f)
-        {
-            chunks[frontTile].setGlobalPosition(10f,5f);
-            frontTile++;
-            if (frontTile > 5)
-            {
-                frontTile = 0;
-            }
-            time = 0f;
-        }
-
-        time = time + 1f / getFPS();
-
-         */
 
     }
 
