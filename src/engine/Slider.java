330634f1c7019cf1d7e5451b72cca70fb03b16eb
Simon <mua.entertainment@outlook.com>
2023-05-25 09:52:39 +0200

    Vegan ist auch ok

--- src/engine/Slider.java
+++ src/engine/Slider.java
@@ -4,10 +4,25 @@ public class Slider extends ImageObject {
 
     private final Button button = new Button();
 
+    private float cursorOriginX;
+
     @Override
     protected void load() {
         super.load();
 
         addChildren(button);
+        button.setSize(1/4f, 1/4f);
+        button.click.subscribe(this::onButtonClick);
+
+        setSize(3f, 1/4f);
+    }
+
+    @Override
+    protected void update() {
+        super.update();
+    }
+
+    private void onButtonClick() {
+        cursorOriginX = getCursorPosition().x;
     }
 }
