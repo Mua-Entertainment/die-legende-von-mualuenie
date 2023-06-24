db8fbb585e54619fbf1a98588190af737d19f0a9
Daronok <github@louistube.de>
2023-06-24 15:21:24 +0200

    jump

--- src/development/Environment.java
+++ src/development/Environment.java
@@ -9,8 +9,6 @@ import java.awt.event.KeyEvent;
 // GameObject in dem alle Objekte sich befinden
 public class Environment extends GameObject {
 
-    private final float ACCELERATION = 20f, JUMP_SPEED = 20f, GROUND_LEVEL = 5f;
-    private float currentJumpSpeed = 0;
     private Label label;
 
     @Override
@@ -31,13 +29,6 @@ public class Environment extends GameObject {
     protected void update() {
         super.update();
 
-        if (getPosition().y < GROUND_LEVEL || currentJumpSpeed > 0) {
-            move(0, -currentJumpSpeed / getFPS());
-            currentJumpSpeed -= ACCELERATION / getFPS();
-        } else {
-            setY(GROUND_LEVEL);
-            currentJumpSpeed = getInput().keyDown(KeyEvent.VK_SPACE) ? JUMP_SPEED : 0;
-        }
 
         label.setGlobalPosition(7, 4);
         label.setText(String.valueOf(getPosition().y));
