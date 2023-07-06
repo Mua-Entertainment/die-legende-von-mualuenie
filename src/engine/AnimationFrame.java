fe93c791b2e47dec261dc15d55742666ed1fc575
Mua-Entertainment <109302434+Mua-Entertainment@users.noreply.github.com>
2023-07-06 10:58:04 +0200

    arne 123 auf fortnite

--- src/engine/AnimationFrame.java
+++
@@ -1,9 +0,0 @@
-// Simo Münc
-
-package engine;
-
-// Frame einer Animation
-// delay: Dauer bis zum nächsten Frame
-// action: Auszuführende Aktion
-public record AnimationFrame(float delay, Runnable action) {
-}
